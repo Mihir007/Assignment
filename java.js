37b3ee07e7e0b171ff7d412f26c20bbfeb1e7a73
Mihir007 <Mihir@Mihirs-MacBook-Pro.local>
2017-01-10 10:33:38 +0000

    corrected my repository

---
+++ java.js
@@ -0,0 +1,10 @@
+// Back To Top
+
+//http://www.webdesignerdepot.com/2014/01/10-jquery-snippets-every-designer-should-know/
+
+$('a.top').click(function(){
+$(document.body).animate({scrollTop : 0},800);
+return false;
+});
+
+
