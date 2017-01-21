06c7b1cd4a4361be63ba767a74553c4392f0d6a0
Mihir007 <Mihir@Mihirs-MacBook-Pro.local>
2017-01-21 16:15:44 +0000

    the social buttons are now replaced by a new footer

--- java.js
+++ java.js
@@ -1,10 +1,48 @@
-// Back To Top
+//https://www.youtube.com/watch?v=JTlI7yqnk4c
 
-//http://www.webdesignerdepot.com/2014/01/10-jquery-snippets-every-designer-should-know/
 
-$('a.top').click(function(){
-$(document.body).animate({scrollTop : 0},800);
-return false;
+function Scroll(){
+$(document).on('scroll', function(){
+    if($(this).scrollTop() > 1){
+        $('header').addClass('sticky');
+    }else{
+        $('header').removeClass('sticky');
+    }
 });
+    
+$(document).on('click', function(){
+    $('html,body').animate({scrollTop:0},500);
+    return false;
+});
+
+// to control the button 
+
+$(document).scroll(function(e){
+   var scrollpos = $(this).scrollTop(); //scrollpos means scroll position 
+    if(scrollpos < 100){
+        $('#top').addClass('hide');
+    }else{
+        $('#top').removeClass('hide');
+    }  
+});
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
