e2686ccfffaff8121109db05dd79efa0ce7e2f99
Mihir007 <Mihir@Mihirs-MacBook-Pro.local>
2017-02-26 15:34:32 +0000

    updated navigation bar and crollcontrol

--- java.js
+++ java.js
@@ -14,13 +14,20 @@ function carousel() {
     setTimeout(carousel, 2000); // Change image every 2 seconds
 }
 
+/*--------------- hash change control------------------------*/
+/* file:///Users/Mihir/Documents/Github/index.html */
+  var shiftWindow = function() { scrollBy(0, -180) };
+    window.addEventListener("hashchange", shiftWindow);
+    function load() { if (window.location.hash) shiftWindow(); }
+
 
 /*--------- below is the script for Google Map API----*/
+/* https://developers.google.com/maps/documentation/javascript/adding-a-google-map  */
 
 function initMap() {
         var uluru = {lat: 52.62973, lng: -1.13897};
         var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 4,
+          zoom: 19,
           center: uluru
         });
         var marker = new google.maps.Marker({
@@ -28,11 +35,6 @@ function initMap() {
           map: map
         });
     }
-
-
-
-
-
 
 
 
