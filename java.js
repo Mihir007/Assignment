920b258f1f498b00055bdef5b357891b29487758
Mihir007 <Mihir@Mihirs-MacBook-Pro.local>
2017-03-09 11:33:26 +0000

    Final Update completed

--- java.js
+++ java.js
@@ -1,28 +1,37 @@
+/*
+
+Author: Mihr Pathak
+*/
+
+
 /*-----Below is the script for the image slider -----*/
 var myIndex = 0;
 carousel();
-
-function carousel() {
+function carousel() { /*initialising the function*/
     var i;
     var x = document.getElementsByClassName("mySlides");
+    /*get's the images from the set class*/
     for (i = 0; i < x.length; i++) {
        x[i].style.display = "none";
+        /*if there is just one image, show that image. no slide */
     }
     myIndex++;
+    /* if there is more image, slide the images in block every 2 seconds */
     if (myIndex > x.length) {myIndex = 1}
     x[myIndex-1].style.display = "block";
     setTimeout(carousel, 2000); // Change image every 2 seconds
 }
 
-/*--------------- hash change control------------------------*/
-/*https://github.com/twbs/bootstrap/issues/1768*/
+/*--------------- hash change control-----------------------*/
+/* https://github.com/twbs/bootstrap/issues/1768
+*/
   var shiftWindow = function() { scrollBy(0, -180) };
     window.addEventListener("hashchange", shiftWindow);
     function load() { if (window.location.hash) shiftWindow(); }
 
 
 /*--------- below is the script for Google Map API----*/
-/* https://developers.google.com/maps/documentation/javascript/adding-a-google-map  */
+/* This is available from the following website:  https://developers.google.com/maps/documentation/javascript/adding-a-google-map  */
 
 function initMap() {
         var uluru = {lat: 52.62973, lng: -1.13897};
